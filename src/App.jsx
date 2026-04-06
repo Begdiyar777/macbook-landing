aa45c18ff2f1a29e35187c00009aee2265499f4e
Begdiyar <jumamuratovbb@gmail.com>
2026-04-06 23:32:26 +0300

    Изменения предыдущего коммита

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all"
+
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
